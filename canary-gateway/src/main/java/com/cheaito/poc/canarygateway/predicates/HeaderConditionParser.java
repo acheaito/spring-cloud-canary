9341a9a7096285e9aade15768b50f6271a92944f
Ali Cheaito <acheaito@epikconsultant.com>
2021-08-27 21:56:39 -0400

    Added header condition parser

---
+++ canary-gateway/src/main/java/com/cheaito/poc/canarygateway/predicates/HeaderConditionParser.java
@@ -0,0 +1,91 @@
+package com.cheaito.poc.canarygateway.predicates;
+
+import org.apache.logging.log4j.util.Strings;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class HeaderConditionParser {
+    private static final List<String> DEFINED_OPERATIONS = Arrays.asList("endsWith", "notEndsWith");
+    private static final String ARGUMENT_SEPARATOR = " ";
+    public static final Pattern ARGUMENT_PATTERN = Pattern.compile("(!?)([\\w].*)\\((.*)\\)");
+    private final String header;
+    private String operation;
+    private List<String> arguments;
+    private Matcher matcher;
+
+    public HeaderConditionParser(String header, String condition) {
+        validateInputNotBlank(header, condition);
+        createConditionMatcher(condition);
+        parseOperation();
+        parseArguments();
+        this.header = header;
+    }
+
+    private void parseArguments() {
+        var argString = matcher.group(3).strip();
+        this.arguments = Arrays.stream(argString.split(ARGUMENT_SEPARATOR))
+                .map(String::strip)
+                .filter(Predicate.not(String::isBlank))
+                .collect(Collectors.toList());
+    }
+
+    private void parseOperation() {
+        this.operation = matcher.group(2);
+        if (isNegated())
+            negateOperation();
+        if (!DEFINED_OPERATIONS.contains(operation))
+            throw new UnsupportedOperationException("Requested operation is not valid: " + operation);
+    }
+
+    private boolean isNegated() {
+        return "!".equals(matcher.group(1));
+    }
+
+    private void negateOperation() {
+        operation = "not" + operation.substring(0,1).toUpperCase() + operation.substring(1);
+    }
+
+    private void createConditionMatcher(String condition) {
+        matcher = ARGUMENT_PATTERN.matcher(condition);
+        if (!matcher.matches())
+            throw new MalformedConditionException("Malformed condition: " + condition);
+    }
+
+    private void validateInputNotBlank(String header, String condition) {
+        if(Strings.isBlank(header))
+            throw new IllegalArgumentException("header cannot be blank");
+        if(Strings.isBlank(condition))
+            throw new IllegalArgumentException("condition cannot be blank");
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public List<String> getArguments() {
+        return this.arguments;
+    }
+
+    public static class UnsupportedOperationException extends RuntimeException{
+        public UnsupportedOperationException(String message) {
+            super(message);
+        }
+    }
+
+    public static class MalformedConditionException extends RuntimeException{
+        public MalformedConditionException(String message) {
+            super(message);
+        }
+    }
+}
